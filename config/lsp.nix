63899276cd8c54e3a505593a8d99867a163489c5
Maxwell Rodgers <mjrodgers05@gmail.com>
2025-02-09 16:01:08 -0700

    updated rust lsp

--- config/lsp.nix
+++ config/lsp.nix
@@ -6,7 +6,11 @@
       clangd.enable = true;        # C/C++
       nixd.enable = true;          # Nix
       pylsp.enable = true;         # Python
-      rust_analyzer.enable = true; # Rust
+      rust-analyzer = {            # Rust
+        enable = true;               # LSP
+        installCargo = true;         # Req. Cargo
+        installRustc = true;         # Req. Rustc
+      };
     };
   };
 }
