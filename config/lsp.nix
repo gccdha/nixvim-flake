50a24c0fe6e2075d9a69b6bcf991f7ddc9854170
Maxwell Rodgers <mjrodgers05@gmail.com>
2025-02-09 15:32:20 -0700

    added rust and python lsps

--- config/lsp.nix
+++ config/lsp.nix
@@ -3,8 +3,10 @@
     enable = true;
     #inlayHints = true;
     servers = {
-      clangd.enable = true;
-      nixd.enable = true;
+      clangd.enable = true;        # C/C++
+      nixd.enable = true;          # Nix
+      pylsp.enable = true;         # Python
+      rust_analyzer.enable = true; # Rust
     };
   };
 }
