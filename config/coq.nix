018a744119809437dd809658d8d975a561d5f835
Maxwell Rodgers <mjrodgers05@gmail.com>
2025-10-01 14:49:57 -0600

    fix coq? try 2

--- config/coq.nix
+++ config/coq.nix
@@ -3,5 +3,11 @@
     enable = true;
     installArtifacts = true;
     settings.xdg = true;
+    package = pkgs.vimPlugins.coq_nvim.overrideAttrs (old: {
+      postPatch = ''
+        substituteInPlace lua/coq/deps.lua \
+          --replace 'assert(_IN_VENV)' 'true'
+      '';
+    });
   };
 }
