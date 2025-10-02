c56f654bd1340b5b11b9afded09b98f629d75016
Maxwell Rodgers <mjrodgers05@gmail.com>
2025-10-01 20:22:31 -0600

    fixed hash in custom theme

--- config/color-schemes.nix
+++ config/color-schemes.nix
@@ -6,7 +6,7 @@
         owner = "utubo";
         repo = "vim-colorscheme-girly";
         rev = "7f8c286c9f85d2000fcc04e79941fd0bce8ee13d";
-        hash = "1xw8ddkvr39fgkjkxdf1fz1z5iqsid0xrq9srp4n346wl0fshr1d";
+        sha265 = "1xw8ddkvr39fgkjkxdf1fz1z5iqsid0xrq9srp4n346wl0fshr1d";
     };
   })];
   colorscheme = "girly";
