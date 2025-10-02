dc608538cb9afe4b1ae0764764d368f2516ca37f
Maxwell Rodgers <mjrodgers05@gmail.com>
2025-10-01 20:11:03 -0600

    external theme test

--- config/color-schemes.nix
+++ config/color-schemes.nix
@@ -1,5 +1,16 @@
-{
+{pkgs,...}:{
+  extraPlugins = [(pkgs.vimUtils.buildVimPlugin {
+    name = "girly";
+    isColorscheme = true;
+    src = pkgs.fetchFromGitHub {
+        owner = "utubo";
+        repo = "vim-colorscheme-girly";
+        rev = "7f8c286c9f85d2000fcc04e79941fd0bce8ee13d";
+        hash = "1xw8ddkvr39fgkjkxdf1fz1z5iqsid0xrq9srp4n346wl0fshr1d";
+    };
+  })];
   colorschemes = {
-    vscode.enable = true;
+    vscode.enable = false;
+    girly.enable = true;
   };
 }
