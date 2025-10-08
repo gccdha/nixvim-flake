38f85ecacbd2d610943390cfe964b1ae806f85a0
Maxwell Rodgers <mjrodgers05@gmail.com>
2025-10-07 22:24:05 -0600

    added LaTeX lsp

--- config/lsp.nix
+++ config/lsp.nix
@@ -7,6 +7,7 @@
       nixd.enable = true;          # Nix
       pylsp.enable = true;         # Python
       metals.enable = true;        # Scala
+      texlab.enable = true;        # LaTeX
       rust_analyzer = {            # Rust
         enable = true;               # LSP
         installCargo = true;         # Req. Cargo
