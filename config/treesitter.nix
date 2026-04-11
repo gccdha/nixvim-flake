7bd23004c09f484d1b49bf2472f870ec778ba9a9
Maxwell Rodgers <mjrodgers05@gmail.com>
2026-04-11 13:12:58 -0600

    added verilog to treesitters

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,7 +1,18 @@
+{ config, ... }:
 {
   plugins.treesitter = {
     enable = true;
-    settings.folding.enable = false;
-    settings.indent.enable = true;
+    folding.enable = false;
+    indent.enable = true;
+    grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
+      bash
+      json
+      make
+      markdown
+      nix
+      regex
+      toml
+      verilog
+    ];
   };
 }
