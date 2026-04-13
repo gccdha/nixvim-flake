35112c059eab5ff29e8e104ceba48cb71ed708ad
Maxwell Rodgers <mjrodgers05@gmail.com>
2026-04-13 10:37:43 -0600

    reverted indent changes; causing errors in other files

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -4,15 +4,15 @@
     enable = true;
     folding.enable = false;
     indent.enable = true;
-    grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
-      bash
-      json
-      make
-      markdown
-      nix
-      regex
-      toml
-      systemverilog
-    ];
+    # grammarPackages = with config.plugins.treesitter.package.builtGrammars; [
+    #   bash
+    #   json
+    #   make
+    #   markdown
+    #   nix
+    #   regex
+    #   toml
+    #   systemverilog
+    # ];
   };
 }
